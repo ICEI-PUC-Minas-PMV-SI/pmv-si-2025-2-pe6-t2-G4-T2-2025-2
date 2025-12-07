23cd01d00821d5ab85f3afaef8ea6f01f5a5e4fa
Thiago Ferreira <thiago.oliveira.1446007@sga.pucminas.br>
2025-12-07 17:10:12 -0300

    Update API base URL to use placeholder IP

--- src/front-end/mobile/flow-app/src/services/api.ts
+++ src/front-end/mobile/flow-app/src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://192.168.0.47:3333" // Inserir o IP do seu computador aqui
+    baseURL: "http://XXX.XXX.X.XXX:3333" // Inserir o IP do seu computador aqui
 });
